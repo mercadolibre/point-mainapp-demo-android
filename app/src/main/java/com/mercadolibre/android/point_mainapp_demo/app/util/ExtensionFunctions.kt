1708c6cc2ae21ac03400b0a9bf5e1a8e1818ba88
Ibrahim Estanga <ibrahim.estanga@mercadolibre.com.co>
2023-05-04 18:53:02 -0500

    new ui paymentflow with payment method

--- app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/util/ExtensionFunctions.kt
+++ app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/util/ExtensionFunctions.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 
 internal fun View.gone() {
     this.visibility = View.GONE
@@ -24,4 +25,8 @@ fun Activity.hideKeyboard() {
 fun Context.hideKeyboard(view: View) {
     val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
     inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+}
+
+fun Context.toast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
--- app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/payment/launcher/PaymentLauncherActivity.kt
+++ app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/payment/launcher/PaymentLauncherActivity.kt
@@ -1,38 +1,75 @@
 package com.mercadolibre.android.point_mainapp_demo.app.view.payment.launcher
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mercadolibre.android.point_integration_sdk.nativesdk.MPManager
+import com.mercadolibre.android.point_integration_sdk.nativesdk.resolver.validate.amount.exception.IllegalAmountException
+import com.mercadolibre.android.point_mainapp_demo.app.R
 import com.mercadolibre.android.point_mainapp_demo.app.databinding.PointMainappDemoAppActivityPaymentLauncherBinding
+import com.mercadolibre.android.point_mainapp_demo.app.util.toast
+import com.mercadolibre.android.point_mainapp_demo.app.view.payment.adapter.PaymentMethodAdapter
+import com.mercadolibre.android.point_mainapp_demo.app.view.payment.models.PaymentMethodModel
 
 /** Main activity class */
 class PaymentLauncherActivity : AppCompatActivity() {
 
-    private var binding: PointMainappDemoAppActivityPaymentLauncherBinding? = null
+    lateinit var binding: PointMainappDemoAppActivityPaymentLauncherBinding
     private val paymentFlow = MPManager.paymentFlow
+    private val paymentTool = MPManager.paymentMethodsTools
+    private var lastPaymentMethodSelected: String? = null
+    private var clearPaymentMethodList: Boolean = true
+    private val paymentMethodAdapter by lazy {
+        PaymentMethodAdapter {
+            lastPaymentMethodSelected = it
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = PointMainappDemoAppActivityPaymentLauncherBinding.inflate(layoutInflater)
-        binding?.run { setContentView(root) }
+        setContentView(binding.root)
+
+        binding.recyclerviewPaymentMethod.apply {
+            layoutManager = LinearLayoutManager(this@PaymentLauncherActivity, LinearLayoutManager.VERTICAL, false)
+            adapter = paymentMethodAdapter
+        }
 
         configPaymentButton()
     }
 
     private fun configPaymentButton() {
-        binding?.sendPaymentActionButton?.setOnClickListener {
-            val amount = binding?.amountEditText?.text?.toString()
-            val description = binding?.descriptionEditText?.text?.toString()
-            if (!amount.isNullOrEmpty()) {
-                launchPaymentFlowIntent(
-                    amount = amount,
-                    description = description,
-                    context = this
-                )
+        binding.apply {
+            getPaymentMethodActionButton.setOnClickListener {
+                clearPaymentMethodList = clearPaymentMethodList.not()
+                if (clearPaymentMethodList) {
+                    getPaymentMethodActionButton.text = getString(R.string.point_mainapp_demo_app_lab_get_payment_method_action)
+                    paymentMethodAdapter.clear()
+                } else {
+                    getPaymentMethodActionButton.text = getString(R.string.point_mainapp_demo_app_clear_label)
+                    configPaymentMethodList()
+                }
+
+            }
+
+            sendPaymentActionButton.setOnClickListener {
+                val amount = binding.amountEditText.text?.toString()
+                val description = binding.descriptionEditText.text?.toString()
+                if (!amount.isNullOrEmpty()) {
+                    launchPaymentFlowIntent(
+                        amount = amount,
+                        description = description,
+                        context = this@PaymentLauncherActivity
+                    )
+                }
             }
         }
+    }
+
+    private fun configPaymentMethodList() {
+        val paymentMethodList = paymentTool.getPaymentMethods().map { PaymentMethodModel(name = it.name) }
+        paymentMethodAdapter.submitList(paymentMethodList)
     }
 
     private fun launchPaymentFlowIntent(
@@ -52,6 +89,10 @@ class PaymentLauncherActivity : AppCompatActivity() {
             hashMapOf("attr" to "456"),
             "demo_app"
         )
-        paymentFlow.launchPaymentFlowActivity(amount, description, uriSuccess, uriError, context)
+        try {
+            paymentFlow.launchPaymentFlowActivity(amount, description, uriSuccess, uriError, context, lastPaymentMethodSelected)
+        } catch (e: IllegalAmountException) {
+            toast(e.message.orEmpty())
+        }
     }
 }
