155e5f31a78c4dea0c3c45cf86a587c4f6448a33
ymrMeli <yeimer.rojas@mercadolibre.com.co>
2023-09-19 10:14:57 -0500

    remove experimental

--- app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/home/HomeActivity.kt
+++ app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/home/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.mercadolibre.android.point_mainapp_demo.app.ActionsProviderImpl
+import com.mercadolibre.android.point_mainapp_demo.app.BuildConfig
+import com.mercadolibre.android.point_mainapp_demo.app.R
 import com.mercadolibre.android.point_mainapp_demo.app.actions.contract.HomeActions
 import com.mercadolibre.android.point_mainapp_demo.app.actions.view.HomeActionAdapter
 import com.mercadolibre.android.point_mainapp_demo.app.databinding.PointMainappDemoAppActivityHomeBinding
@@ -24,6 +26,13 @@ class HomeActivity : AppCompatActivity() {
         binding.run { setContentView(root) }
         actionAdapter.submitList(ActionsProviderImpl.getActions(this@HomeActivity))
         setRecyclerView()
+        getVersionName()
+    }
+
+    private fun getVersionName() {
+        val versionName = BuildConfig.VERSION_NAME
+        binding.pointMainappDemoAppVersion.text =
+            getString(R.string.point_mainapp_demo_app_version_name, versionName)
     }
 
     private fun setRecyclerView() {
