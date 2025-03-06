a918d163688317a60415e46cd14f6633a4aebdb3
Yeimer Rojas Ballesteros <yeimer.rojas@mercadolibre.com.co>
2025-03-06 09:20:53 -0500

    addeing funcionality get payment status (#66)

---
+++ app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/payment/result/PaymentStatusApprovedActivity.kt
@@ -0,0 +1,72 @@
+package com.mercadolibre.android.point_mainapp_demo.app.view.payment.result
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import com.mercadolibre.android.point_integration_sdk.nativesdk.MPManager
+import com.mercadolibre.android.point_integration_sdk.nativesdk.message.utils.doIfError
+import com.mercadolibre.android.point_integration_sdk.nativesdk.message.utils.doIfSuccess
+import com.mercadolibre.android.point_integration_sdk.nativesdk.payment.data.PaymentResponse
+import com.mercadolibre.android.point_mainapp_demo.app.databinding.ActivityPaymentStatusApprovedBinding
+import com.mercadolibre.android.point_mainapp_demo.app.util.visible
+
+class PaymentStatusApprovedActivity : AppCompatActivity() {
+
+    private var binding: ActivityPaymentStatusApprovedBinding? = null
+
+    private val paymentStatus = MPManager.paymentStatus
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPaymentStatusApprovedBinding.inflate(layoutInflater)
+        binding?.run { setContentView(root) }
+        searchPayment()
+        goBack()
+    }
+
+    private fun searchPayment() {
+        binding?.apply {
+            searchInputPaymentStatusApproved.setEndIconOnClickListener {
+                val paymentId = searchInputPaymentStatusApproved.editText?.text.toString()
+                getPaymentStatus(paymentId)
+            }
+        }
+    }
+
+    private fun getPaymentStatus(paymentId: String) {
+        paymentStatus.getPaymentStatus(paymentId) { response ->
+            response.doIfSuccess {
+                paymentStatusSuccessResult(it)
+            }
+            response.doIfError {
+                paymentStatusErrorResult(it.message)
+            }
+        }
+    }
+
+    private fun paymentStatusSuccessResult(paymentResponse: PaymentResponse) {
+        binding?.apply {
+            cardPaymentStatusApproved.visible()
+            textPaymentAmountResult.text = paymentResponse.paymentAmount.toString()
+            textPaymentInstallmentResult.text = paymentResponse.paymentInstallments
+            textPaymentStatusPaymentMethodResult.text = paymentResponse.paymentMethod.toString()
+            textPaymentStatusPaymentReferenceResult.text = paymentResponse.paymentReference
+            textPaymentStatusBrandNameResult.text = paymentResponse.paymentBrandName
+            textPaymentStatusCreationDateResult.text = paymentResponse.paymentCreationDate
+            textPaymentStatusLastFourDigitsResult.text = paymentResponse.paymentLastFourDigits
+        }
+    }
+
+    private fun paymentStatusErrorResult(message: String?) {
+        Snackbar.make(binding!!.root, message ?: "Error", Snackbar.ANIMATION_MODE_SLIDE)
+            .setBackgroundTint(getColor(android.R.color.holo_red_light)).show()
+    }
+
+    private fun goBack() {
+        binding?.apply {
+            buttonPaymentStatusApproved.setOnClickListener {
+                finish()
+            }
+        }
+    }
+}
