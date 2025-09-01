0142cb257d59255fb3ccf4b0321e837f907aaf0f
dalvaradorus_meli <danielfelipe.alvaradorussi@mercadolibre.com.co>
2025-09-01 10:00:58 -0500

    feat implementation bump version 5.0.3

--- app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/payment/launcher/PaymentLauncherActivity.kt
+++ app/src/main/java/com/mercadolibre/android/point_mainapp_demo/app/view/payment/launcher/PaymentLauncherActivity.kt
@@ -12,6 +12,7 @@ import com.mercadolibre.android.point_integration_sdk.nativesdk.message.utils.do
 import com.mercadolibre.android.point_integration_sdk.nativesdk.payment.data.PayerCondition
 import com.mercadolibre.android.point_integration_sdk.nativesdk.payment.data.PaymentFlowRequestData
 import com.mercadolibre.android.point_integration_sdk.nativesdk.payment.data.PaymentMethod
+import com.mercadolibre.android.point_integration_sdk.nativesdk.payment.data.SubMerchantData
 import com.mercadolibre.android.point_mainapp_demo.app.R
 import com.mercadolibre.android.point_mainapp_demo.app.databinding.PointMainappDemoAppActivityPaymentLauncherBinding
 import com.mercadolibre.android.point_mainapp_demo.app.util.gone
@@ -138,7 +139,24 @@ class PaymentLauncherActivity : AppCompatActivity() {
                 description = description,
                 paymentMethod = lastPaymentMethodSelected,
                 printOnTerminal = isPrintOnTerminal,
-                taxes = binding.payerCondition.getSelectedValue()?.toTaxes()
+                taxes = binding.payerCondition.getSelectedValue()?.toTaxes(),
+                subMerchantData = SubMerchantData(
+                    subMerchantId = 123,
+                    mcc = "123123123",
+                    country = "BRA",
+                    addressDoorNumber = 23,
+                    zip = "03710120",
+                    documentNumber = "40792973860",
+                    city = "São Paulo",
+                    addressStreet = "Rua",
+                    legalName = "Legal Name",
+                    regionCodeIso = "SP",
+                    regionCode = "11",
+                    documentType = "CPF",
+                    fiscalCondition = "CONSUMIDOR_FINAL",
+                    phone = "11999999999",
+                    url = "www.google.com"
+                )
             )
         ) { response ->
             binding.paymentProgressBar.gone()
